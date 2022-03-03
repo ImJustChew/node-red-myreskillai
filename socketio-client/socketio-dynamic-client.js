4a2b76cbdde576daa1d17f8acca5dfea6ca3f182
Chew Tzi Hwee <chewtzihwee@gmail.com>
2022-03-03 17:19:26 +0800

    converted inference into displayable base64 and output

--- socketio-client/socketio-dynamic-client.js
+++ socketio-client/socketio-dynamic-client.js
@@ -1,85 +1,95 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
   'use strict';
   // var io = require('socket.io-client');
   var sockets = {};
 
   /* sckt connector*/
-    function ReskillAIConnector(n){
-      RED.nodes.createNode(this, n);
-      this.name = n.name;
-      var node = this;
-      
-      if(sockets[node.id]){ delete sockets[node.id];}
-      sockets[node.id] = connect();
-        
-      sockets[node.id].on('connect', function(){
-        sockets[node.id].emit('user','node-red')
-        node.send({ payload:{socketId:node.id, status:'connected'} });
-        node.status({fill:"green",shape:"dot", text:"connected"});
-      });
+  function ReskillAIConnector(n) {
+    RED.nodes.createNode(this, n);
+    this.name = n.name;
+    var node = this;
 
-      sockets[node.id].on('disconnect', function(){
-        node.send({payload:{socketId:node.id, status:'disconnected'}});
-        node.status({fill:'red',shape:'ring', text:'disconnected'});
-      });
+    if (sockets[node.id]) { delete sockets[node.id]; }
+    sockets[node.id] = connect();
 
-      sockets[node.id].on('connect_error', function(err) {
-        if (err) {
-          node.status({fill:'red',shape:'ring',text:'disconnected'});
-          node.send({payload:{socketId:node.id, status:'disconnected'}});
-          //node.error(err);
-        }
-      }); 
+    sockets[node.id].on('connect', function () {
+      sockets[node.id].emit('user', 'node-red')
+      node.send({ payload: { socketId: node.id, status: 'connected' } });
+      node.status({ fill: "green", shape: "dot", text: "connected" });
+    });
 
-      this.on('close', function(done) {
-        sockets[node.id].disconnect();
-        node.status({});
-        done();
-      }); 
+    sockets[node.id].on('disconnect', function () {
+      node.send({ payload: { socketId: node.id, status: 'disconnected' } });
+      node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
+    });
+
+    sockets[node.id].on('connect_error', function (err) {
+      if (err) {
+        node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
+        node.send({ payload: { socketId: node.id, status: 'disconnected' } });
+        //node.error(err);
+      }
+    });
+
+    this.on('close', function (done) {
+      sockets[node.id].disconnect();
+      node.status({});
+      done();
+    });
+  }
+  RED.nodes.registerType('reskilai-connector', ReskillAIConnector);
+
+  function arrayBufferToBase64(buffer) {
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
     }
-    RED.nodes.registerType('reskilai-connector', ReskillAIConnector);
+    return binary;
+  }
 
-    /* inference listener*/
-    function InferenceListener(n){
-      RED.nodes.createNode(this, n);
-      this.name = n.name;
-      this.socketId = null;
-      var node = this;
-      node.on('input', function(msg){
-        node.socketId = msg.payload.socketId;
-        if(msg.payload.status == 'connected'){
-          node.status({fill:'green',shape:'dot',text:'listening'});
-          if( !sockets[node.socketId].hasListeners('inference') ){
-            sockets[node.socketId].on('inference', function(data){
-              node.send( {payload:data} );
-            });
-          }
-        } else {
-          node.status({fill:'red',shape:'ring',text:'disconnected'});
-          if( sockets[node.socketId].hasListeners(node.eventName) ){
-            sockets[node.socketId].removeListener(node.eventName, function(){});
-          }
-        }
-      });
-
-      node.on('close', function(done) {
-        if( sockets[node.socketId].hasListeners(node.eventName) ){
-          sockets[node.socketId].removeListener(node.eventName, function(){
-            node.status({});
-            done();
+  /* inference listener*/
+  function InferenceListener(n) {
+    RED.nodes.createNode(this, n);
+    this.name = n.name;
+    this.socketId = null;
+    var node = this;
+    node.on('input', function (msg) {
+      node.socketId = msg.payload.socketId;
+      if (msg.payload.status == 'connected') {
+        node.status({ fill: 'green', shape: 'dot', text: 'listening' });
+        if (!sockets[node.socketId].hasListeners('inference')) {
+          sockets[node.socketId].on('inference', function (data) {
+            node.send({ payload: "data:image/png;base64," + arrayBufferToBase64(data) });
           });
-        }else{
+        }
+      } else {
+        node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
+        if (sockets[node.socketId].hasListeners(node.eventName)) {
+          sockets[node.socketId].removeListener(node.eventName, function () { });
+        }
+      }
+    });
+
+    node.on('close', function (done) {
+      if (sockets[node.socketId].hasListeners(node.eventName)) {
+        sockets[node.socketId].removeListener(node.eventName, function () {
           node.status({});
           done();
-        }
-            
-      }); 
-    }
-    RED.nodes.registerType('reskillai-inference', InferenceListener);
+        });
+      } else {
+        node.status({});
+        done();
+      }
+
+    });
+  }
+  RED.nodes.registerType('reskillai-inference', InferenceListener);
 
   function connect() {
     var uri = 'ws://localhost:12345'; //TODO: FOR ACTUAL DEPLOYMENT USE RESKILLAI>LOCAL
     var options = {};
-    return require('socket.io-client')( uri, options );;
+    return require('socket.io-client')(uri, options);;
   }
 }
